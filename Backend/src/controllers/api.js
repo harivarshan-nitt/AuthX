95104eae2285f3ca6a141ea6d18ef8d299311ff7
SANTHOSH17-DOT <santhoshm12052003@gmail.com>
2023-04-26 23:31:11 +0530

    Added: resource route - User data Updated: directory structure

--- Backend/src/controllers/api.js
+++ Backend/src/controllers/api.js
@@ -1,5 +1,8 @@
+const verifyJWT = require('../middlewares/verifyJWT');
+
 const api = require('express').Router();
 
-api.use("/clientApp",require('../routes/clientApp'))
+api.use("/clientApp", require('../routes/clientApp'))
 api.use("/employee", require("../routes/employee"))
+api.use("/user", verifyJWT, require('../routes/userData'))
 module.exports = api
--- Backend/src/routes/accessToken.js
+++ Backend/src/routes/accessToken.js
@@ -1,36 +1,6 @@
 const router = require("express").Router();
-const { createJWT } = require("../helpers/jwt");
-const Client = require("../models/schema/clientApp");
+const exchangeAuthCode = require("../services/exchangeAuthCode");
 
 // Exchanging auth code for access token to access user data
-router.post("/", async(req, res) => {
-    try {
-        const auth_code = req.body.auth_code
-        const clientAppId = req.body.clientAppId
-        const clientSecert = req.body.clientSecret
-        const client = await Client.findOne({ clientAppId: clientAppId })
-        if (client == null) {
-            return res.status(404).json({
-                message: "Client App does not exist"
-            })
-        }
-        if (clientSecert != client.clientAppSecret) {
-            return res.status(403).json({
-                message: "Unauthorized access"
-            })
-        }
-        const user = client.users.find(user => user.auth_code == auth_code)
-        const access_token = createJWT({ id: user.user._id })
-        return res.status(200).json({
-            message: "Access token generated successfully",
-            scope: client.scopes,
-            access_token
-        })
-    } catch (err) {
-        console.log(err)
-        return res.status(500).json({
-            message: "Internal server error"
-        })
-    }
-})
+router.post("/", exchangeAuthCode)
 module.exports = router
